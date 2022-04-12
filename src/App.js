473bf8f8b7504b738f8779620488023e6f703046
Kendal Gates <kendalgates80@gmail.com>
2022-04-12 16:29:28 +0200

    aded setInputText to use State

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 //Importing components
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
      <header>My React To-Do-List</header>
-     <Form />
+     <Form setInputText={setInputText}/>
      <TodoList />
     </div>
   );
